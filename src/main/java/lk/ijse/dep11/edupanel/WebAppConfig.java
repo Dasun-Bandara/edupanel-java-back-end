9964d0e6fe413da4778fe0c550c1e497fd8f434a
Dasun Bandara <dasun8517@gmail.com>
2023-12-05 10:18:39 +0530

    Finish createNewLecturer web service

--- src/main/java/lk/ijse/dep11/edupanel/WebAppConfig.java
+++ src/main/java/lk/ijse/dep11/edupanel/WebAppConfig.java
@@ -1,10 +1,19 @@
 package lk.ijse.dep11.edupanel;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Bucket;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.StorageClient;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 @EnableWebMvc
@@ -12,7 +21,22 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 public class WebAppConfig {
 
     @Bean
+    public Bucket defaultBucket() throws IOException {
+        InputStream serviceAccount = getClass().getResourceAsStream("/edupannel-firebase-adminsdk-k17ct-c4d23b07f7.json");
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setStorageBucket("edupannel.appspot.com")
+                .build();
+        FirebaseApp.initializeApp(options);
+
+        return StorageClient.getInstance().bucket();
+
+    }
+
+    @Bean
     public StandardServletMultipartResolver multipartResolver(){
         return new StandardServletMultipartResolver();
     }
+
 }
--- src/main/java/lk/ijse/dep11/edupanel/WebRootConfig.java
+++ src/main/java/lk/ijse/dep11/edupanel/WebRootConfig.java
@@ -1,9 +1,27 @@
 package lk.ijse.dep11.edupanel;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
+@PropertySource("classpath:/application.properties")
 public class WebRootConfig {
+
+    @Bean(destroyMethod = "close")
+    public HikariDataSource dataSource(Environment env) {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(env.getRequiredProperty("spring.datasource.url"));
+        config.setUsername(env.getRequiredProperty("spring.datasource.username"));
+        config.setPassword(env.getRequiredProperty("spring.datasource.password"));
+        config.setDriverClassName(env.getRequiredProperty("spring.datasource.driver-class-name"));
+        config.setMaximumPoolSize(env.getRequiredProperty("spring.datasource.hikari.maximum-pool-size",
+                Integer.class));
+        return new HikariDataSource(config);
+    }
 }
